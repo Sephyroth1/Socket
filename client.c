5e6e37729e1f41c8eca3c2e441eea0ac6dbb17a6
Sephyroth1 <sudhamshuamperayani@gmail.com>
2023-10-21 16:33:52 +0530

    Updating the repo.

--- client.c
+++ client.c
@@ -3,14 +3,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
-
+#include <string.h>
 
 int main(int argc, char *argv[]){
 
 	WSADATA wsaData; 	
-	int iResult, status;
+	int iResult;
 	int sockfd, connectfd;
-	//char buffer[70];
+	char buffer[1024];
 
 	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
     if (iResult != 0) {
@@ -18,35 +18,7 @@ int main(int argc, char *argv[]){
         return 1;
     }
 
-	struct addrinfo serv, *info = NULL;
-
-	char ipbuffer[40];
-	char *name = "127.0.0.1", *type = "http";
-
-	memset(&serv, 0, sizeof(serv));
-
-	serv.ai_family = AF_INET;
-	serv.ai_protocol = IPPROTO_TCP;
-	serv.ai_socktype = SOCK_STREAM;
-
-	status = getaddrinfo(name, type, &serv, &info);
-	if (status != 0){
-		fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
-		WSACleanup();
-		return 1;
-}
-
-	void *addr1;
-
-	printf("Reached point A\n");
-
-	struct sockaddr_in *ipv4 = (struct sockaddr_in * )info->ai_addr;
-	addr1 = &(ipv4->sin_addr);
-	inet_ntop(info->ai_family, addr1, ipbuffer, sizeof(ipbuffer));
-
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-	printf("Reached point B\n");
 
 	if(sockfd < 0){
 		perror("socket");
@@ -59,7 +31,6 @@ int main(int argc, char *argv[]){
 	addr.sin_port = htons(8080);
 	inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr);
 	socklen_t bytes = sizeof(addr);
-	printf("Reached point C\n");
 
 	connectfd = connect(sockfd, (struct sockaddr *)&addr, bytes);
 	if(connectfd == -1){
@@ -68,23 +39,25 @@ int main(int argc, char *argv[]){
 		WSACleanup();
 		return -1;
 	}
-	printf("%d \n", connectfd);
+	printf("%d is the error_code\n", connectfd);
 	printf("Socket connected successfully\n");
 
-	/*int bytes = recv(sockfd, buffer, sizeof(buffer), 0);
-	if(bytes == -1){
-		printf("%d \n", bytes);
+	int bytes_send = recv(sockfd, buffer, sizeof(buffer), 0);
+	if(bytes_send == -1){
+		printf("%d \n", bytes_send);
 		perror("recv");
 		WSACleanup();
 
-	}else if(bytes == 0){
-		printf("Connected was closed by server");
+	}else if(bytes_send == 0){
+		printf("Connected was closed by server\n");
 	}else{
-		printf("Data Received Successfully");
-		printf("%s\n = received data", buffer);
-	}*/
+		printf("Data Received Successfully\n");
+		printf("%s = received data", buffer);
+	}
+
+	char *buffers = "Hello World to you too!";
+	send(sockfd, buffers, strlen(buffers), 0);
 	closesocket(sockfd);
-	freeaddrinfo(info);
 	WSACleanup();
 	return 0;
 }
--- server.c
+++ server.c
@@ -3,27 +3,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <string.h>
 
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
 
 	WSADATA wsaData;
-	int iResult, status;
-	int attach, listenfd, backlog = 10, acceptfd;
+	int iResult;
+	int attach, listenfd, backlog = 3, acceptfd;
 	SOCKET sockfd;
+
 	/*int receivefd, len = 100;
 	char buff[100];*/
-
+	
 	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-    if (iResult != 0) {
-        printf("WSAStartup failed: %d\n", iResult);
-        return 1;
-    }
+	if (iResult != 0) {
+		printf("WSAStartup failed: %d\n", iResult);
+		return 1;
+	}
 
-	struct addrinfo serv, *info = NULL;
+	struct addrinfo serv;
 
 	char clientIp[40];
-	char *name = "127.0.0.1", *type = "http";
 
 	memset(&serv, 0, sizeof(serv));
 
@@ -31,53 +32,42 @@ int main(int argc, char *argv[]){
 	serv.ai_protocol = IPPROTO_TCP;
 	serv.ai_socktype = SOCK_STREAM;
 
-	status = getaddrinfo(name, type, &serv, &info);
-	if (status != 0){
-		fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
-		WSACleanup();
-		return 1;
-	}
 
-	sockfd = socket(info->ai_family, info->ai_socktype, info->ai_protocol);
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-	if(sockfd < 0){
+	if (sockfd < 0) {
 		fprintf(stderr, "socket error: %s\n", gai_strerror(sockfd));
 		WSACleanup();
 		return 1;
 	}
-	struct sockaddr_in addr, claddr;
-	socklen_t bytes = sizeof(struct sockaddr_in);
+	struct sockaddr_in addr;
+	socklen_t bytes = sizeof(addr);
 	memset(addr.sin_zero, '\0', sizeof(addr.sin_zero));
 	addr.sin_family  = AF_INET;
 	addr.sin_port = htons(8080);
 	inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr);
 
-	memset(claddr.sin_zero, '\0', sizeof(claddr.sin_zero));
-	claddr.sin_family = AF_INET;
-	claddr.sin_port = htons(8080);
-	inet_pton(AF_INET, "127.0.0.1", &claddr.sin_addr);
 
-	attach = bind(sockfd, info->ai_addr, bytes);
-	if(attach < 0){
+	attach = bind(sockfd, (struct sockaddr *)&addr, bytes);
+	if (attach < 0) {
 		perror("bind error");
 		fprintf(stderr, "%d \n", errno);
 		WSACleanup();
 		return 1;
 	}
 
-	printf("Socket Bound successfully");
+	printf("Socket Bound successfully\n");
 	listenfd = listen(sockfd, backlog);
-	if(listenfd != 0){
+	if (listenfd != 0) {
 		perror("listen: ");
 		WSACleanup();
 		return 1;
 	}
-	//clientsocket = socket(AF_INET, SOCK_STREAM, 0);
 
-	socklen_t addr_size = sizeof(struct sockaddr_in);
-	acceptfd = accept(sockfd, (struct sockaddr *)&claddr, &addr_size);
+	socklen_t addr_size = sizeof(addr);
+	acceptfd = accept(sockfd, (struct sockaddr *)&addr, &addr_size);
 
-	if(acceptfd == -1){
+	if (acceptfd == -1) {
 		printf("%d \n", acceptfd);
 		perror("accept got the error");
 		printf("%d: errno, %d: acceptfd", errno, acceptfd);
@@ -85,11 +75,27 @@ int main(int argc, char *argv[]){
 		WSACleanup();
 	}
 
-	inet_ntop(AF_INET, &(claddr.sin_addr), clientIp, sizeof(clientIp));
-	printf("Connection accepted from the client: %s\n%d", clientIp, claddr.sin_port);
+	char *Hello = "Hello World";
+	send(acceptfd, Hello, strlen(Hello), 0);
+
+	inet_ntop(AF_INET, &(addr.sin_addr), clientIp, sizeof(clientIp));
+	printf("Connection accepted from the client: %s\n %d\n", clientIp, addr.sin_port);
+	
+	char buffer1[1024];
+
+	int bytes_send = recv(acceptfd, buffer1, strlen(buffer1), 0);
+	if(bytes_send == -1){
+		printf("%d \n", bytes_send);
+		perror("recv");
+		WSACleanup();
+	}else if(bytes_send == 0){
+		printf("Connection was closed by client\n");
+	}else{
+		printf("Data Received successfully\n");
+		printf("%s = received data", buffer1);
+	}
 
 	closesocket(sockfd);
-	freeaddrinfo(info);
 	WSACleanup();
 	return 0;
 }
